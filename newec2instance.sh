df34fb4defe0994d8644e3102e775b6e4bddd10c
Rocky05 <36295686+Rocky05@users.noreply.github.com>
2021-08-14 15:54:41 +0530

    reversed changes

--- newec2instance.sh
+++ newec2instance.sh
@@ -1,4 +1,3 @@
 echo "-----Server Creation-----"
 
-sudo su -
 aws ec2 run-instances --image-id  ami-0443305dabd4be2bc --instance-type t2.micro --key-name Linux_key --security-groups default --region us-east-2
