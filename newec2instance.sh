e949c15732e809eb26f4fa7816274f084977c389
root <root@ip-172-31-9-212.us-east-2.compute.internal>
2021-08-14 07:36:38 +0000

    added echo

--- newec2instance.sh
+++ newec2instance.sh
@@ -1,3 +1,5 @@
 echo "-----Server Creation-----"
 
+echo "---new tes----"
+
 aws ec2 run-instances --image-id  ami-0443305dabd4be2bc --instance-type t2.micro --key-name Linux_key --security-groups default --region us-east-2
