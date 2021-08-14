b6d4db9594a9b2ee5b9b7913e4233040ff12cc1d
Rocky05 <36295686+Rocky05@users.noreply.github.com>
2021-08-14 15:51:05 +0530

    Updated file

--- newec2instance.sh
+++ newec2instance.sh
@@ -1,7 +1,3 @@
 echo "-----Server Creation-----"
 
-echo "---new tes----"
-
-echo "---test2---"
-
 aws ec2 run-instances --image-id  ami-0443305dabd4be2bc --instance-type t2.micro --key-name Linux_key --security-groups default --region us-east-2
