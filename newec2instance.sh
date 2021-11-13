55bf9e76ceb14fb8ed5e577206d973c52afe21d3
Rocky05 <36295686+Rocky05@users.noreply.github.com>
2021-11-14 00:24:31 +0530

    Key update

--- newec2instance.sh
+++ newec2instance.sh
@@ -1,3 +1,3 @@
 echo "-----Server Creation-----"
 
-aws ec2 run-instances --image-id  ami-0dd0ccab7e2801812 --instance-type t2.micro --key-name Linux_key --security-groups default --region us-east-2
+aws ec2 run-instances --image-id  ami-0dd0ccab7e2801812 --instance-type t2.micro --key-name Nov_13 --security-groups default --region us-east-2
